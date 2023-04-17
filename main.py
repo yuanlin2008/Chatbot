a0fad198b8047674ba9ad57056ceb644ac409614
yuanlin2008 <yuanlin2008@hotmail.com>
2023-04-17 16:34:51 +0800

    123

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import typer
 from typing import List
 import html2text
+import os
+import subprocess
 
 import scraper
 import database
@@ -53,6 +55,23 @@ def clean(all:bool = False):
     h2t.ignore_images = True
     database.clean(h2t.handle, all)
     database.close()
+
+@app.command()
+def update_model():
+    """
+    从github更新模型
+    """
+    if os.path.exists('./model'):
+        subprocess.run(['git', 'pull'], cwd='./model')
+    else:
+        os.makedirs('./model')
+        subprocess.run(['git', 'clone', 'https://github.com/THUDM/ChatGLM-6B'], cwd='./model')
+    os.system('pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118')
+    os.system('pip install -r ./model/ChatGLM-6B/requirements.txt')
+    
+@app.command()
+def test():
+    subprocess.run(['python', 'cli_demo.py'], cwd='./model/ChatGLM-6B')
 
 if __name__ == "__main__":
     app()
